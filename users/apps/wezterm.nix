69a2e852f16d2ef850854f5d6992ddac54cc8944
Alexander Trost <galexrt@googlemail.com>
2023-08-05 15:47:25 +0200

    use different color scheme for terminal
    Signed-off-by: Alexander Trost <galexrt@googlemail.com>

--- users/apps/wezterm.nix
+++ users/apps/wezterm.nix
@@ -9,10 +9,10 @@
           'Noto Color Emoji',
         },
         font_size = 8.0,
-        color_scheme = "Ibm 3270 (High Contrast) (Gogh)",
-        colors = {
-          background = '#282a36',
-        },
+        color_scheme = "Solarized Dark Higher Contrast (Gogh)",
+        --colors = {
+        --  background = '#282a36',
+        --},
         enable_tab_bar = false,
         window_background_opacity = 0.96,
         enable_scroll_bar = true,
