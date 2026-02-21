38d3ae497c73ff23443376888f08548fc945a1d4
Alexander Trost <galexrt@googlemail.com>
2026-02-21 12:49:34 +0100

    fix: upgrade kernel to 6.19
    Signed-off-by: Alexander Trost <galexrt@googlemail.com>

--- base.nix
+++ base.nix
@@ -15,7 +15,7 @@
   boot.swraid.enable = true;
 
   # Linux kernel
-  boot.kernelPackages = pkgs.linuxPackagesFor (pkgs.linuxKernel.kernels.linux_6_18);
+  boot.kernelPackages = pkgs.linuxPackagesFor (pkgs.linuxKernel.kernels.linux_6_19);
 
   hardware = {
     enableAllFirmware = true;
@@ -179,6 +179,7 @@
     iotop
     jq
     lact
+    linuxKernel.packages.linux_6_19.cpupower
     lm_sensors
     lsof
     mtr
