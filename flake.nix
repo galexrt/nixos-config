0c2a1a873dbacb08553354ed4756eeae1719fe0d
Alexander Trost <galexrt@googlemail.com>
2026-06-02 16:58:18 +0200

    chore: bump to 26.05 and run nix flake update
    Signed-off-by: Alexander Trost <galexrt@googlemail.com>

--- flake.nix
+++ flake.nix
@@ -9,7 +9,7 @@
     };
 
     nixpkgs = {
-      url = "github:NixOS/nixpkgs/nixos-25.11";
+      url = "github:NixOS/nixpkgs/nixos-26.05";
     };
     nixos-unstable = {
       url = "github:NixOS/nixpkgs/nixos-unstable";
@@ -23,7 +23,7 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
     home-manager = {
-      url = "github:nix-community/home-manager/release-25.11";
+      url = "github:nix-community/home-manager/release-26.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
