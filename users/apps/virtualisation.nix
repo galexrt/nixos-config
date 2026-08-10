e427549657e71a254d4e5865a1c0c0b697c31cb9
Alexander Trost <galexrt@googlemail.com>
2026-08-10 16:37:15 +0200

    fix: remove libvirtd verbatim config section
    Signed-off-by: Alexander Trost <galexrt@googlemail.com>

--- users/apps/virtualisation.nix
+++ users/apps/virtualisation.nix
@@ -32,17 +32,6 @@
         package = pkgs.qemu_kvm;
         runAsRoot = true;
         swtpm.enable = true;
-
-        verbatimConfig = ''
-          namespaces = []
-          cgroup_device_acl = [
-            "/dev/null", "/dev/full", "/dev/zero",
-            "/dev/random", "/dev/urandom",
-            "/dev/ptmx", "/dev/kvm", "/dev/kqemu",
-            "/dev/rtc","/dev/hpet", "/dev/vfio/vfio",
-            "/dev/kvmfr0"
-          ]
-        '';
       };
     };
   };
