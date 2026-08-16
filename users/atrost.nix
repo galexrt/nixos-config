04c8817391662e2eb40525ad6d2587627289c30d
Alexander Trost <galexrt@googlemail.com>
2026-08-16 21:05:54 +0200

    feat: update vscode config and add user to disk group
    Signed-off-by: Alexander Trost <galexrt@googlemail.com>

--- users/atrost.nix
+++ users/atrost.nix
@@ -46,6 +46,7 @@
       "dialout"
       "libvirtd"
       "gamemode"
+      "disk"
     ]; # Enable ‘sudo’ for the user.
     openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIACcqbQlcdBFswQafVSTt0OvMkBLwXjTSLhBsqAdo5Gf atrost@debwrk01"
